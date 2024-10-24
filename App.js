21b23b285563ddbc927cd66cdea6e77439946da9
Gabriel Fortes Ashikaga <gabriel.ashikaga@outlook.com>
2024-10-23 21:31:55 -0300

    :sparkles: Segunda parte da aula

--- App.js
+++ App.js
@@ -1,35 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import Jobs from "./src/components/Jobs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+// Criando o Stack Navigator
+
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [nome, setNome] = useState("Profº Helder");
-
-  let img = "https://sujeitoprogramador.com/steve.png";
-
-  const handleMudar = () => {
-    setNome("Prof Helder Pestana");
-  };
 
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "#FF0000", fontSize: 25, margin: 15 }}>
-        Programador
-      </Text>
-      <Jobs img={img} largura={100} altura={200} fulano="Steve Jobs" />
-      <Text style={{ fontSize: 30 }}>{nome}</Text>
-      <Button title="Entrar" onPress={handleMudar} />
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" >
+        {/*Definindo a tela "Home" */}
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        {/*Definindo a tela "Details" */}
+        <Stack.Screen name="Details" component={DetailsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
